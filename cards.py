b09d1ef188da5f9252727f1b3309451b54927de0
Diekope <geekman973@gmail.com>
2023-03-02 15:58:51 -0300

    Ajout premier code

---
+++ cards.py
@@ -0,0 +1,19 @@
+class Card:
+    #Création des cartes
+    def __init__(self,color="white",number=0,special_ability=None):
+        self.color = color
+        self.number = number
+        self.special_ability = special_ability
+
+    #Affichage des cartes
+    def __str__(self):
+        return f"{self.get_card_text()} - {self.color}"
+
+    def __repr__(self):
+        return f"{self.get_card_text()} - {self.color}"
+
+    def get_card_text(self):
+        if self.number == 0:
+            return self.special_ability
+        else:
+            return self.number
